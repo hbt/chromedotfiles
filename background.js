5f466d701c1f434f6d3b2f2897c0f69ea1b2b232
hbt <hassenbentanfous@gmail.com>
2018-12-08 02:43:46 -0500

    reduced CSS injection latency further
    Previous API was using chrome.tabs.insertCSS with a file
    this uses ajax
    Surprisingly, this is faster. No visible delays when loading youtube. No
    white flashes

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
 'use strict';
 
+function listenShortcutReloadExtensionOnDemand() {
+  chrome.commands.onCommand.addListener(function (command) {
+    switch (command) {
+      case 'restartext':
+        chrome.runtime.reload();
+        break;
+    }
+  });
+}
+
+
 function getLocation(href) {
   var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)(\/[^?#]*)(\?[^#]*|)(#.*|)$/);
   return match && {
@@ -13,69 +24,91 @@ function getLocation(href) {
   };
 }
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  var match = getLocation(tab.url);
+function injectJS(tabId, match) {
+// attempt to execute default js
+  chrome.tabs.executeScript(tabId, {
+    file: 'chromedotfiles/default.js'
+  }, function (res) {
+    if (chrome.runtime.lastError) {
+      // file not found, fail silently
+      return;
+    }
+  });
 
-  // load css early for no visible delays
-  if(changeInfo.status === 'loading') {
-    
-    // attempt to insert domain specific css
-    chrome.tabs.insertCSS(tabId, {
-      file: 'chromedotfiles/default.css',
-      runAt: 'document_start',
-      allFrames: true
+  if (match) {
+    // attempt to execute domain specific js
+    chrome.tabs.executeScript(tabId, {
+      file: 'chromedotfiles/' + match.hostname + '.js'
     }, function (res) {
       if (chrome.runtime.lastError) {
         // file not found, fail silently
         return;
       }
     });
-
-    if (match) {
-      // attempt to insert domain specific css
-      chrome.tabs.insertCSS(tabId, {
-        file: 'chromedotfiles/' + match.hostname + '.css',
-        runAt: 'document_start',
-        allFrames: true
-      }, function(res) {
-        if (chrome.runtime.lastError) {
-          // file not found, fail silently
-          return;
-        }
-      });
-    }
   }
-  
-  
-  if (changeInfo.status === 'complete') {
+}
+
+function injectCSS(tabId, match) {
+
+  function injectCSSFile(tabId, filename) {
+
+    let curl = chrome.runtime.getURL(`chromedotfiles/${filename}.css`)
+    try {
+      $.ajax({
+        url: curl
+      }).done(function (data) {
+        if (data) {
+          chrome.tabs.insertCSS(
+            tabId,
+            {
+              code: data,
+              runAt: "document_start",
+              allFrames: true
+            },
+            function (res) {
+            }
+          );
+        }
+      }).error(function() {
+        
+      });
+    } catch(e) {
+      
+    }
     
-    // attempt to execute default js
-    chrome.tabs.executeScript(tabId, {
-      file: 'chromedotfiles/default.js'
-    }, function(res) {
-      if (chrome.runtime.lastError) {
-        // file not found, fail silently
-        return;
-      }
-    });
 
-    if (match) {
-      // attempt to execute domain specific js
-      chrome.tabs.executeScript(tabId, {
-        file: 'chromedotfiles/' + match.hostname + '.js'
-      }, function(res) {
-        if (chrome.runtime.lastError) {
-          // file not found, fail silently
-          return;
-        }
-      });
-    }
   }
-});
+
+
+  injectCSSFile(tabId, 'default')
+  if (match) {
+    injectCSSFile(tabId, match.hostname)
+  }
+
+
+}
+
+function main() {
+  listenShortcutReloadExtensionOnDemand()
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    var match = getLocation(tab.url);
+
+    if (changeInfo.status === 'loading') {
+      injectCSS(tabId, match);
+    }
+
+
+    if (changeInfo.status === 'complete') {
+      injectJS(tabId, match);
+    }
+  });
+
+}
+
 
 /**
  * simulates loading JS files in order passed in array and in sync mode instead of async
- * 
+ *
  * @param tabId
  * @param files
  * @param callbackFunctionName
@@ -111,9 +144,12 @@ function loadJSFile(tabId, files, callbackFunctionName) {
 function loadJSFiles(filepath, callbackFunctionName, tab) {
 
   var files = filepath;
-  if(!(filepath instanceof Array)) {
+  if (!(filepath instanceof Array)) {
     files = [filepath];
   }
-  
+
   loadJSFile(tab.id, files, callbackFunctionName);
 }
+
+
+main()
